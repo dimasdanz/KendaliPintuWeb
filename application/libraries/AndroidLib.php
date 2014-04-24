eb4dc81c9b16007099604fc27378c3f791879719
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-25 03:39:42 +0700

    Add GNU CPLv3 license header

--- application/libraries/AndroidLib.php
+++ application/libraries/AndroidLib.php
@@ -1,4 +1,22 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+/*******************************************************************************
+ * Copyright (c) 2014 Dimas Rullyan Danu.
+ * 
+ * This file is part of Kendali Pintu
+ * 
+ * Kendali Pintu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Kendali Pintu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Kendali Pintu. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 class AndroidLib{
 	public function send_notification($user_id = '', $message = '', $input_source = ''){
